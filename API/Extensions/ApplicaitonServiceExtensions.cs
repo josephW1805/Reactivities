c629ce4d6f5e15982e9e9b40907e162761546b76
josephW1805 <joseph.weng.1805@gmail.com>
2022-02-09 16:57:26 -0500

    End of section 19

--- API/Extensions/ApplicaitonServiceExtensions.cs
+++ API/Extensions/ApplicaitonServiceExtensions.cs
@@ -28,7 +28,7 @@ namespace API.Extensions
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins(new string[] { "http://localhost:3000", "test" });
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins(new string[] { "http://localhost:3000", "test" });
                 });
             });
             services.AddMediatR(typeof(List.Handler).Assembly);
@@ -36,6 +36,7 @@ namespace API.Extensions
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+            services.AddSignalR();
 
             return services;
         }
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -174,18 +174,26 @@ export default class ActivityStore {
     }
   };
 
-  cancelActivityToggle =async () => {
+  cancelActivityToggle = async () => {
     this.loading = true;
     try {
       await agent.Activities.attend(this.selectedActivity!.id);
       runInAction(() => {
-        this.selectedActivity!.isCancelled = !this.selectedActivity?.isCancelled;
-        this.activityRegistry.set(this.selectedActivity!.id, this.selectedActivity!);
-      })
+        this.selectedActivity!.isCancelled =
+          !this.selectedActivity?.isCancelled;
+        this.activityRegistry.set(
+          this.selectedActivity!.id,
+          this.selectedActivity!
+        );
+      });
     } catch (error) {
       console.log(error);
     } finally {
-      runInAction(() => this.loading = false);
+      runInAction(() => (this.loading = false));
     }
-  }
+  };
+
+  clearSelectedActivity = () => {
+    this.selectedActivity = undefined;
+  };
 }
